5b0117ef5bc2a4978a39d1ee2d5b04e0af89e395
vanilla-dd <freeidlll6@gmail.com>
2024-05-16 14:53:23 +0530

    feat: auth setup completed

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$lib/server/db';
+import github from '@auth/sveltekit/providers/github';
+import { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET } from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	adapter: DrizzleAdapter(db),
-	providers: []
+	providers: [github({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })],
+	pages: { signIn: '/signin' }
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
+</script>
+
+<SignIn provider="github" signInPage="signin" />
+
+<SignOut />
+
+{JSON.stringify($page.data.session?.user)}
